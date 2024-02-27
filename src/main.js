03681313d43f322a0e80c033daf97d79dafbf007
Gugun Gunawan <gugun.net21@gmail.com>
2024-02-27 07:30:29 +0700

    add crud

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+// //import config router
+// import router from './router'
+// //create App Vue
+// const app = createApp(App);
 
-createApp(App).mount('#app')
+// //gunakan "router" di Vue dengan plugin "use"
+// app.use(router);
+
+// createApp(App).mount('#app')
+
+
+
+//import createApp from Vue
+import { createApp } from 'vue';
+
+//import component App
+import App from './App.vue';
+
+//import config router
+import router from './router'
+
+//create App Vue
+const app = createApp(App);
+
+//gunakan "router" di Vue dengan plugin "use"
+app.use(router);
+
+app.mount('#app');
